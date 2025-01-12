eb2490b437590f4619cdfff811b0b2729e2d02ef
mutapakkir <151417931+mutapakkir@users.noreply.github.com>
2025-01-12 07:10:16 -0800

    Add files via upload

--- routes/movie-routes.js
+++ routes/movie-routes.js
@@ -1,1 +1,8 @@
+const express = require('express')
+const movieController = require('../controllers/movie-controllers')
+const router = express.Router()
 
+router.get('/', movieController.searchMovie)
+router.get('/popular', movieController.getPopularMovies);
+
+module.exports = router
